ce3096d5e93941da5e8288e519faa91576240014
ababuu <ababualemu123@gmail.com>
2021-09-21 10:31:05 +0300

    function to change letters to digits added

---
+++ tests/caesarCipher.js
@@ -0,0 +1,16 @@
+
+function encrypt(key,message){
+
+}
+
+function decrypt(key,message){
+
+}
+function lettersToDigits(message){
+    const letters=message.split('');
+    console.log(letters);
+}
+lettersToDigits('ababu');
+
+
+module.exports= {encrypt,decrypt};
